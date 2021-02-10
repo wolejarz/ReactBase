354e969ba449c2eb50cc718978fd275b01145816
Wojciech Olejarz <wojciech.olejarz@onet.eu>
2021-02-10 14:24:57 +0100

    onChange works

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 
 class App extends React.Component {
   state = {
-    value: ""
+    value: "123"
   }
+
+  handleInputChange(e)  {
+    console.log(e.target.value);
+    this.setState({value: e.target.value});
+  }  
   render() {
     return (
       <React.Fragment>
-        <input type="text"/>
+        <input placeholder="Enter ..." onChange = {this.handleInputChange.bind(this)} type="text"/>
         <button>Reset</button>
-        <h1> </h1>
+        <h1 className='title'> {this.state.value} </h1>
       </React.Fragment>
     )
   }
